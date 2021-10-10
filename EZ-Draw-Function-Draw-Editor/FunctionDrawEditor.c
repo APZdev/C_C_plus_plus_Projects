41ad503939f253da78870c2ce75dc35d5b4168fd
Adam <44951876+APZdev@users.noreply.github.com>
2021-10-10 07:17:42 +0200

    Finished project exercise 6

--- EZ-Draw-Function-Draw-Editor/FunctionDrawEditor.c
+++ EZ-Draw-Function-Draw-Editor/FunctionDrawEditor.c
@@ -15,18 +15,31 @@
 
 #include "ez-draw.h"
 #include <math.h>
-#define CurvePrecision 0.001
+#define CurvePrecision 0.01
+#define StoredCurveArraySize (int)(200 * (1 / CurvePrecision) * 2)
 
-typedef struct {
+typedef struct 
+{
     int unitInPixelsX, unitInPixelsY, graphOffsetX, graphOffsetY, lastPosX, lastPosY;
 } NavigationValues;
 
-typedef struct {
-    float curveValues[(int)(1 + (50 * (1 / CurvePrecision) * 2))];
+typedef struct 
+{
+    double curveValuesX[StoredCurveArraySize];
+    double curveValuesY[StoredCurveArraySize];
+    unsigned short valueExists[StoredCurveArraySize];
+    int storedCurveSize;
 } StoredCurve;
+
+typedef struct 
+{
+    int width, height;
+    int mouseX, mouseY;
+} WindowInfos;
 
 StoredCurve storedCurve;
 NavigationValues navigationValues;
+WindowInfos windowInfos;
 
 void drawCaretAtPosition(Ez_window win, int x, int y, int onHorizontalAxis)
 {
@@ -38,51 +51,71 @@ void drawCaretAtPosition(Ez_window win, int x, int y, int onHorizontalAxis)
         ez_draw_line(win, x - caretSize, y, x + caretSize, y);
 }
 
-double getFunctionImage(double x, int* testCasePassed)
+double getFunctionImage(double x, int* outTestCasePassed)
 {
     //Check if the sqrt content is always > 0 to prevent any error
     double testCase = 1 + ((x / 2) * (x + sqrt(pow(x, 2) + 8)));
     if (testCase > 0)
     {
-        *testCasePassed = 1;
+        *outTestCasePassed = 1;
         return sqrt(testCase);
     }
     else
     {
-        *testCasePassed = 0;
+        *outTestCasePassed = 0;
         return 0;
     }
 }
 
-//TODO : Store the array values instead of calculating the cruve every render
+//Calculate and store curve data
+void getFunctionData(int width, int unitInPixelsX)
+{
+    int curveSizeCounter = 0;
+    for (double i = -200; i <= 200; i += CurvePrecision)
+    {
+        int testCaseResult; //Store the test case result to prevent rendering a point that doesn't exist, for example when dividing by 0
+
+        //Store results
+        storedCurve.curveValuesY[curveSizeCounter] = getFunctionImage(i, &testCaseResult);
+        storedCurve.valueExists[curveSizeCounter] = (testCaseResult > 0);
+        storedCurve.curveValuesX[curveSizeCounter] = i;
+
+        curveSizeCounter++;
+    }
+
+    //After the loop, we have the final stored values count of the array
+    storedCurve.storedCurveSize = curveSizeCounter;
+}
+
+//Draw the function representative curve
 void drawFunction(Ez_window win, int width, int height, int unitInPixelsX, int unitInPixelsY, int windowOriginOffsetX, int windowOriginOffsetY)
 {
-    //Draw the function representative curve
-    ez_set_thick(3.5);
+    //Set curve color and thickness
+    ez_set_thick(2.5);
     ez_set_color(ez_red);
 
-    int sectionAmountX = (int)(width / unitInPixelsX);
-    int arrayCounter = 0;
-    for (double i = -(int)(sectionAmountX / 2); i <= (int)(sectionAmountX / 2); i += CurvePrecision)
+    int isFirstDraw = 1;
+    int caretAmountX = (int)(width / unitInPixelsX) + abs((int)(windowOriginOffsetX / unitInPixelsX));
+    for (int i = (int)((storedCurve.storedCurveSize / 2) - (caretAmountX * (1 / CurvePrecision))); i < ((storedCurve.storedCurveSize / 2) + (caretAmountX * (1 / CurvePrecision))) - 2; i++)
     {
-        int testCaseResult, testCaseNextResult; //Store the test case result to prevent rendering a point that doesn't exist
+        //Get the point coordinates from the stored curve values
+        double x = storedCurve.curveValuesX[i];
+        double y = storedCurve.curveValuesY[i];
+        double yNext = storedCurve.curveValuesY[i + 1];
 
-        //Get result of current point and next to draw a line
-        double result = getFunctionImage(i, &testCaseResult);
-        double nextResult = getFunctionImage(i + CurvePrecision, &testCaseNextResult);
-
-        if (testCaseResult > 0 && testCaseNextResult > 0) //Draw line only if both test case passed
+        //Draw the lines
+        if (storedCurve.valueExists[i] == 1)
         {
-            storedCurve.curveValues[(int)(abs(i * CurvePrecision))] = result;
-
-            char numStr[25];
-            snprintf(numStr, 50, "%d -> %f", (int)(abs(i * CurvePrecision)), storedCurve.curveValues[(int)(abs(i * CurvePrecision))]);
-            ez_draw_text(win, EZ_BC, 70, 30, numStr);
-
-            ez_draw_line(win, (int)((width / 2 ) + windowOriginOffsetX + (i * unitInPixelsX)), (int)((height / 2) + windowOriginOffsetY - (result * unitInPixelsY)),
-                (int)((width / 2) + windowOriginOffsetX + ((i + CurvePrecision) * unitInPixelsX)), (int)((height / 2) + windowOriginOffsetY - (nextResult * unitInPixelsY)));
+            ez_draw_line(win, (int)((width / 2 ) + windowOriginOffsetX + (x * unitInPixelsX)), (int)((height / 2) + windowOriginOffsetY - (y * unitInPixelsY)),
+                    (int)((width / 2) + windowOriginOffsetX + ((x + CurvePrecision) * unitInPixelsX)), (int)((height / 2) + windowOriginOffsetY - (yNext * unitInPixelsY)));
         }
-        arrayCounter++;
+        else if (storedCurve.valueExists[i] == 0 && storedCurve.valueExists[i + 1] == 1 && isFirstDraw == 1) //First draw check to prevent failed testCases not rendering the first line
+        {
+            isFirstDraw = 0;
+
+            ez_draw_line(win, (int)((width / 2) + windowOriginOffsetX + (x * unitInPixelsX)), (int)((height / 2) + windowOriginOffsetY - (0 * unitInPixelsY)),
+                (int)((width / 2) + windowOriginOffsetX + ((x + CurvePrecision) * unitInPixelsX)), (int)((height / 2) + windowOriginOffsetY - (yNext * unitInPixelsY)));
+        }
     }
 }
 
@@ -95,12 +128,14 @@ void drawAxisLines(Ez_window win, int width, int height, int unitInPixelsX, int 
     ez_draw_line(win, (int)((width / 2) + windowOriginOffsetX), 0, (int)((width / 2) + windowOriginOffsetX), height); //Vertical
 
 
-    //Draw carets
-    int caretAmountX = (int)((width + windowOriginOffsetX) / unitInPixelsX);
+    //Draw carets + numbers
+    int offsetCountX = (int)(windowOriginOffsetX / unitInPixelsX);
+    int caretAmountX = (int)(width / unitInPixelsX) + abs(offsetCountX);
     for (int i = -caretAmountX; i <= caretAmountX; i++)
     {
         drawCaretAtPosition(win, (int)(width / 2) + windowOriginOffsetX + (i * unitInPixelsX), windowOriginOffsetY + (int)(height / 2), 1);
 
+        //Dislay axis numbers
         if (i != 0) //Don't display 0 coordinate
         {
             char numStr[5];
@@ -109,7 +144,8 @@ void drawAxisLines(Ez_window win, int width, int height, int unitInPixelsX, int 
         }
     }
 
-    int caretAmountY = (int)((height + windowOriginOffsetY) / unitInPixelsY);
+    int offsetCountY = (int)(windowOriginOffsetY / unitInPixelsY);
+    int caretAmountY = (int)(height / unitInPixelsY) + abs(offsetCountY);
     for (int i = -caretAmountY; i <= caretAmountY; i++)
     {
         drawCaretAtPosition(win, (int)(width / 2) + windowOriginOffsetX, windowOriginOffsetY + (i * unitInPixelsY) + (int)(height / 2), 0);
@@ -123,16 +159,35 @@ void drawAxisLines(Ez_window win, int width, int height, int unitInPixelsX, int 
     }
 }
 
+void drawCurveHoverPoint(Ez_window win)
+{
+    ez_set_color(ez_blue);
+    ez_set_thick(7);
+
+    char numStr[25];
+    int testCaseResult;
+    double relativeMouseX = (windowInfos.mouseX - (windowInfos.width / 2));
+    double curveImage = getFunctionImage((relativeMouseX - navigationValues.graphOffsetX) / navigationValues.unitInPixelsX, &testCaseResult);
+    if (testCaseResult == 1)
+    {
+        snprintf(numStr, 50, "f(%.3lf) = %lf", (relativeMouseX - navigationValues.graphOffsetX) / navigationValues.unitInPixelsX, curveImage);
+        ez_draw_point(win, (windowInfos.width / 2) + relativeMouseX, (windowInfos.height / 2) - ((curveImage * navigationValues.unitInPixelsY) - navigationValues.graphOffsetY));
+
+    }
+    else
+        snprintf(numStr, 50, "f(%.3lf) = ERROR", (relativeMouseX - navigationValues.graphOffsetX) / navigationValues.unitInPixelsX, curveImage);
+
+    ez_set_nfont(1);
+    ez_draw_text(win, EZ_TL, 30, 30, numStr);
+}
+
 void win1_on_expose(Ez_event* ev)
 {
-    int width = 0, height = 0;
-    ez_window_get_size(ev->win, &width, &height);
+    ez_window_get_size(ev->win, &windowInfos.width, &windowInfos.height);
 
-    int windowOriginOffsetX = navigationValues.graphOffsetX;
-    int windowOriginOffsetY = navigationValues.graphOffsetY;
-
-    drawAxisLines(ev->win, width, height, navigationValues.unitInPixelsX, navigationValues.unitInPixelsY, windowOriginOffsetX, windowOriginOffsetY);
-    drawFunction(ev->win, width, height, navigationValues.unitInPixelsX, navigationValues.unitInPixelsY, windowOriginOffsetX, windowOriginOffsetY);
+    drawAxisLines(ev->win, windowInfos.width, windowInfos.height, navigationValues.unitInPixelsX, navigationValues.unitInPixelsY, navigationValues.graphOffsetX, navigationValues.graphOffsetY);
+    drawFunction(ev->win, windowInfos.width, windowInfos.height, navigationValues.unitInPixelsX, navigationValues.unitInPixelsY, navigationValues.graphOffsetX, navigationValues.graphOffsetY);
+    drawCurveHoverPoint(ev->win);
 }
 
 void increaseGraphScale(Ez_window win)
@@ -145,7 +200,7 @@ void increaseGraphScale(Ez_window win)
 
 void decreaseGraphScale(Ez_window win)
 {
-    if (!(navigationValues.unitInPixelsX - 10 <= 0))
+    if (!(navigationValues.unitInPixelsX - 10 <= 0)) //Check zoom limit before apply
     {
         navigationValues.unitInPixelsX -= 10;
         navigationValues.unitInPixelsY -= 10;
@@ -156,27 +211,26 @@ void decreaseGraphScale(Ez_window win)
 
 void win1_on_motion_notify(Ez_event* ev) /* Mouse moved */
 {
-    /*
-    //Print mouse window cordinates
-    char numStr[25];
-    snprintf(numStr, 50, "x : %d, y : %d", ev->mx, ev->my);
-    ez_draw_text(ev->win, EZ_BC, 70, 30, numStr);
-    */
+    //Update mouse window coordinates
+    windowInfos.mouseX = ev->mx;
+    windowInfos.mouseY = ev->my;
 
     //Move graph on click drag
     if (ev->mb >= 1)
     {
+        //Clamp the view translation
+        if (navigationValues.graphOffsetX + ev->mx - navigationValues.lastPosX <= -5000 || navigationValues.graphOffsetX + ev->mx - navigationValues.lastPosX >= 5000 ||
+            navigationValues.graphOffsetY + ev->my - navigationValues.lastPosY <= -5000 || navigationValues.graphOffsetY + ev->my - navigationValues.lastPosY >= 5000) return;
+
+        //Get the delta mouse movements
         navigationValues.graphOffsetX += ev->mx - navigationValues.lastPosX;
         navigationValues.graphOffsetY += ev->my - navigationValues.lastPosY;
-        
-        navigationValues.lastPosX = ev->mx;
-        navigationValues.lastPosY = ev->my;
     }
-    else
-    {
-        navigationValues.lastPosX = ev->mx;
-        navigationValues.lastPosY = ev->my;
-    }
+
+    //To get the delta we need the previous pos, so we save it
+    navigationValues.lastPosX = ev->mx;
+    navigationValues.lastPosY = ev->my;
+
     ez_send_expose(ev->win);
 }
 
@@ -184,7 +238,7 @@ void win1_on_motion_notify(Ez_event* ev) /* Mouse moved */
 void win1_on_key_press(Ez_event* ev)
 {
     switch (ev->key_sym) {
-    case XK_q: ez_quit(); break;
+    case XK_Escape: ez_quit(); break;
     case XK_p: increaseGraphScale(ev->win); break;
     case XK_m: decreaseGraphScale(ev->win); break;
     }
@@ -207,6 +261,8 @@ int main ()
     Ez_window window;
     window = ez_window_create (1280, 720, "FunctionDrawEditor", win1_on_event);
 
+    ez_window_get_size(window, &windowInfos.width, &windowInfos.height);
+
     navigationValues.unitInPixelsX = 90;
     navigationValues.unitInPixelsY= 90;
     navigationValues.graphOffsetX = 0;
@@ -214,8 +270,9 @@ int main ()
 
     //TODO : Try to somehow lock window size
 
-    //If screen buffer is On, don't resize window every frame
-    ez_window_dbuf(window, 1);
+    ez_window_dbuf(window, 1); //If screen buffer is On, don't resize window every frame
+
+    getFunctionData(windowInfos.width, navigationValues.unitInPixelsX); //Calculate function data once
 
     ez_main_loop ();
     exit(0);
